2722e22e4759cd77cd02afa45c63486577d10e85
Robert Bain <31903812+bainro@users.noreply.github.com>
2023-03-10 13:03:56 -0800

    Update driftdiffusion_rev.m

--- driftdiffusion_rev.m
+++ driftdiffusion_rev.m
@@ -12,6 +12,8 @@ ntrials = 500; %NUMBER OF RUNS
 criterion = 1; %CORRECT BOUNDARY LOCATION, INCORRECT IS ZERO 
 beta = 0.5; % NORMALIZED BIAS
 bias = beta*criterion; %ACTUAL BIAS
+total_err_pr = 0; % keep running sum of err probability
+total_err_rt = 0; % use total_err_pr to normalize this value
 %set random number seed 
 rng(19680104);
 %OUTPUT VARIABLES
@@ -19,31 +21,44 @@ sample = zeros(1,nsteps+1);   %This is a single random draw from normal distribu
 path = zeros(ntrials,nsteps+1); %This is all the random walks
 rt = zeros(ntrials,1);  %These are the rts across trials 
 correct = zeros(ntrials,1); %This is accuracy data. ZERO IS WRONG, ONE IS RIGHT
-%LOOP OVER ntrials.  
-for j = 1:ntrials
-    goodpath = 0;
-    while goodpath == 0
-        draw = normrnd(mu*dt,sd*sqrt(dt),[1,nsteps]);  %DRAW A WALK
-        sample(1) = bias; %START AT BIAS
-        sample(2:nsteps+1) = draw; 
-        walk = cumsum(sample); %SUM THE WALK.   
-        crossbnd = find((walk > criterion) |(walk < 0)); %TEST BOTH BOUNDARIES  
-        if ~isempty(crossbnd) %TEST IF IT CROSSED ONE OF THE BOUNDARIES AT LEAST
-            goodpath = 1; %WALK IS GOOD, SET TO 1 TO EXIT WHILE LOOP
-            path(j,:) = walk; %SAVE THE WALK
-        else
-            display('Bad Walk') %NOTIFY BAD WALK AND DRAW AGAIN LOWER
+
+% loop over all 100 samples of drift rate (i.e. mu)
+for i = 1:100
+    sampled_mu = normrnd(mu,0.1,[1,1])
+    %LOOP OVER ntrials.  
+    for j = 1:ntrials
+        goodpath = 0;
+        while goodpath == 0
+            draw = normrnd(sampled_mu*dt,sd*sqrt(dt),[1,nsteps]);  %DRAW A WALK
+            sample(1) = bias; %START AT BIAS
+            sample(2:nsteps+1) = draw; 
+            walk = cumsum(sample); %SUM THE WALK.   
+            crossbnd = find((walk > criterion) |(walk < 0)); %TEST BOTH BOUNDARIES  
+            if ~isempty(crossbnd) %TEST IF IT CROSSED ONE OF THE BOUNDARIES AT LEAST
+                goodpath = 1; %WALK IS GOOD, SET TO 1 TO EXIT WHILE LOOP
+                path(j,:) = walk; %SAVE THE WALK
+            else
+                display('Bad Walk') %NOTIFY BAD WALK AND DRAW AGAIN LOWER
+            end;
         end;
-    end;
-    rt(j) = crossbnd(1);  %RT IS FIRST CROSSING
-    if path(j,rt(j)) > criterion  %TEST IF CORRECT
-        path(j,rt(j):end) = criterion; %SET THE REST OF WALK TO BOUNDARY
-        correct(j) = 1; %INDICATE CORRECT TRIAL
-    else %TRIALIS INCORRECT
-        path(j,rt(j):end) = 0; %SET THE REST OF WALK TO ZERO. 
-    end; 
-    %Add Non-decision time
-    rt(j) = rt(j) + ndt/dt;	
+        rt(j) = crossbnd(1);  %RT IS FIRST CROSSING
+        if path(j,rt(j)) > criterion  %TEST IF CORRECT
+            path(j,rt(j):end) = criterion; %SET THE REST OF WALK TO BOUNDARY
+            correct(j) = 1; %INDICATE CORRECT TRIAL
+        else %TRIALIS INCORRECT
+            path(j,rt(j):end) = 0; %SET THE REST OF WALK TO ZERO. 
+        end; 
+        %Add Non-decision time
+        rt(j) = rt(j) + ndt/dt;	
+    end
+    % need to weight average incorrect response times by relative probabilities of incorrect
+    %convert rt to milliseconds
+    rt = rt*dt;
+    errorrt = rt(find(correct == 0));
+    avg_err_rt = mean(errorrt);
+    err_pr = 1 - mean(correct);
+    total_err_pr += err_pr;
+    total_err_rt += avg_err_rt;
 end
 
 %compute accuracy
