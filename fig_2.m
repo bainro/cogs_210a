cf3288857621de5148b76d7e2892ff146eb4282b
Robert Bain <31903812+bainro@users.noreply.github.com>
2023-03-10 13:41:04 -0800

    Update fig_2.m

--- fig_2.m
+++ fig_2.m
@@ -27,7 +27,7 @@ for i_ = 1:100
     path = zeros(ntrials,nsteps+1); %This is all the random walks
     rt = zeros(ntrials,1);  %These are the rts across trials 
     correct = zeros(ntrials,1); %This is accuracy data. ZERO IS WRONG, ONE IS RIGHT
-    sampled_beta = normrnd(0.5,0.05,[1,1]);
+    sampled_beta = normrnd(0.5,0.2,[1,1]);
     sampled_bias = sampled_beta*criterion; % ACTUAL BIAS
     %LOOP OVER ntrials.  
     for j = 1:ntrials
